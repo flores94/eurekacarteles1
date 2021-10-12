fd6488d2e3eec115d6a5cbfd1b64a1595a3fd5d6
Flores94 <fndisenio@gmail.com>
2021-10-12 13:45:57 -0300

    [ADD] modificamos el header

---
+++ 404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EUREKA CARTELES - Diseño gráfico y cartelería</title>
+    <meta name="description" content="Estudio de diseño y Gráfica, nos encargamos de elaborar toda tu iimagen coorporativa
+    soluciones integrales para tu campaña publicitaria.">
+    <meta name="keywords" content="tucuman, diseño, grafica, grafico, carteles, publicidad, logos, argentina, desarrolloweb,
+    letrascorporeas, vinilos, ploteos, letreros, banner, estudio">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,800;1,800&display=swap"
+        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/inicio.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+</head>
+<body>
+    <div class="container-404">
+        <h2>ERROR 404</h2>
+        <P>Está pagina no se encuentra disponible en estos momentos.
+            disculpa las molestias.</P>
+            <a href="#index.html" class="boton">
+                PAGINA PRINCIPAL
+            </a>
+            <a href="index.html">
+                <img src="img/gif-gato1.gif" alt="">
+            </a>
+    </div>
+</body>
